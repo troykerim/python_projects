7f1353c97094bccd4a7aa06773c368e2d7150da4
troykerim <troykerim@gmail.com>
2025-04-12 17:23:59 -0700

    Adding a project

---
+++ midpoint.py
@@ -0,0 +1,10 @@
+print("Midpoint finder")
+x1 = float(input("Enter x1 coord: "))
+y1 = float(input("Enter y1 coord: "))
+x2 = float(input("Enter x2 coord: "))
+y2 = float(input("Enter y2 coord: "))
+
+m1 = (x1 + x2) / 2 
+m2 = (y1 + y2) / 2 
+
+print(f"The midpoint is: ({m1},{m2})")
