607df279b32a5c30c9f6abd3b73fe532886b45e6
troykerim <troykerim@gmail.com>
2025-04-17 21:02:03 -0700

    Adding a project

---
+++ rainbow.py
@@ -0,0 +1,41 @@
+import time, sys
+
+try:
+    import bext 
+except ImportError:
+    print("This program requires the bext module")
+    sys.exit()
+    
+print("Rainbow")
+print("Press ctrl-c to stop.")
+time.sleep(3)
+
+indent = 0 
+indentIncreasing = True 
+try:
+    while True:
+        print(' ' * indent, end='')
+        bext.fg('red')
+        print('##', end='')
+        bext.fg('yellow')
+        print('##', end='')
+        bext.fg('green')   
+        print('##', end='')
+        bext.fg('blue')   
+        print('##', end='')
+        bext.fg('cyan')   
+        print('##', end='')
+        bext.fg('purple')
+        print("##")
+        
+        if indentIncreasing:
+            indent = indent + 1
+            if indent == 60:
+                indentIncreasing = False
+        else:   
+            indent = indent -1 
+            if indent == 0:
+                indentIncreasing = True 
+        time.sleep(0.02)
+except KeyboardInterrupt:
+    sys.exit()
