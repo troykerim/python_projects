75957e4f723e8aab43bffcbc463c7f1c1146f7d5
troykerim <troykerim@gmail.com>
2025-07-18 23:11:39 -0700

    Adding project

---
+++ hex_grid.py
@@ -0,0 +1,14 @@
+
+# Set up the constants:
+# (!) Try changing these values to other numbers:
+X_REPEAT = 19  # How many times to tessellate horizontally.
+Y_REPEAT = 5  # How many times to tessellate vertically.
+for y in range(Y_REPEAT):
+    # Display the top half of the hexagon:
+    for x in range(X_REPEAT):
+        print(r'/ \_', end='')
+    print()
+    # Display the bottom half of the hexagon:
+    for x in range(X_REPEAT):
+        print(r'\_/ ', end='')
+    print()
