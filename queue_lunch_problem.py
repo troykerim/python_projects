7972b502873a82d28c344591c5cad5633cd83970
troykerim <troykerim@gmail.com>
2025-08-17 20:22:18 -0700

    Adding project

--- queue_lunch_problem.py
+++ queue_lunch_problem.py
@@ -2,3 +2,16 @@
 Number of students eating.  Leetcode 
 
 '''
+from collections import Counter
+def countStudents(students, sandwiches):
+    res = len(students)
+    count = Counter(students)
+    
+    for s in sandwiches:
+        if count[s] > 0:
+            res -= 1
+            count[s] -= 1
+        else:
+            break
+    return res 
+
