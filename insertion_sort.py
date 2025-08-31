9ed6b45e6526a3096249bb55ecb66899a8e4f3c2
troykerim <troykerim@gmail.com>
2025-08-31 14:45:51 -0700

    Adding Project

---
+++ insertion_sort.py
@@ -0,0 +1,27 @@
+# Python program for implementation of Insertion Sort
+
+# Function to sort array using insertion sort
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# A utility function to print array of size n
+def printArray(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+# Driver method
+if __name__ == "__main__":
+    arr = [55, 11, 13, 2, 6]
+    insertionSort(arr)
+    printArray(arr)
