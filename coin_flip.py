039d65a89aaf38609e70cdf6074b16d0beb7c341
troykerim <troykerim@gmail.com>
2025-09-18 22:25:05 -0700

    Adding project

---
+++ coin_flip.py
@@ -0,0 +1,14 @@
+import random
+import itertools
+
+results = {
+    'heads': 0,
+    'tails': 0,
+}
+sides = list(results.keys())
+
+for i in range(10000):
+    results[random.choice(sides)] += 1
+
+print('Heads:', results['heads'])
+print('Tails:', results['tails'])
